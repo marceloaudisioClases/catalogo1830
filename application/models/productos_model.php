5e16d1d17a9bc6318fad9b705ab8c49745e095fb
marceloaudisioClases <marcelo@hilet.com>
2024-11-07 19:30:28 -0300

    Update productos_model.php

--- application/models/productos_model.php
+++ application/models/productos_model.php
@@ -1,20 +1,10 @@
 <?php
 class Productos_model extends CI_Model {
 
-    public function listar(){
-        $this->db->select("*");
-        
-        $this->db->where("estado",1);
-        $this->db->order_by("orden");
-        $this->db->order_by("nombre");
-        return $this->db->get("categorias")->result_array();
-    }
-
     public function obtener_por_id($id){
         $this->db->select("*");
-        $this->db->where("categoria_id",$id);
+        $this->db->where("producto_id",$id);
         $this->db->limit(1);
-        return $this->db->get("categorias")->row_array();       
+        return $this->db->get("productos")->row_array();       
     }
-
 }
