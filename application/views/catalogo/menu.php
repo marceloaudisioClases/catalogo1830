6a87d0a5e2da7dcf9a9f617576918620fde5ba30
marceloaudisioClases <marcelo@hilet.com>
2024-11-12 19:33:43 -0300

    agregue nuevo menu

--- application/views/catalogo/menu.php
+++ application/views/catalogo/menu.php
@@ -19,6 +19,8 @@
             <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="#"><i class="bi bi-people-fill"></i> Usuarios</a></li>
                 <li><a class="dropdown-item" href="#"><i class="bi bi-person-fill-lock"></i> Roles</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="<?php echo site_url("categorias"); ?>"><i class="bi bi-box"></i> Categorias</a></li>
             </ul>
             </li>
         <?php 
