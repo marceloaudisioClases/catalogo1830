d93857491844f4bb41c296d39d1aeb27f332cabe
marceloaudisioClases <marcelo@hilet.com>
2024-12-05 19:39:29 -0300

    Ejemplo correo

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -10,4 +10,21 @@ class Usuarios extends CI_Controller {
 		$datos["usuarios"] = $this->usuarios_model->listar();
 		$this->load->view('usuarios',$datos);
 	}
+
+	public function send_test() {
+        $this->load->library('email');
+        $this->email->initialize();
+
+        $this->email->from('correo Ejemplo', 'Sistema');
+        $this->email->to('correo ejemplo');
+        $this->email->subject('Test');
+        $this->email->message('Es una Prueba');
+
+        if ($this->email->send()) {
+            echo 'Email Enviado';
+        } else {
+            echo 'Falló al enviar email';
+            echo $this->email->print_debugger();
+        }
+    }
 }
